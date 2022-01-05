7c4217c5de915b155fe24b5a4cab97343635680b
Noah Weaver <noahweaver@aol.com>
2022-01-05 17:25:03 -0600

    successfully used context with typescript

--- client/src/context/UserProvider.tsx
+++ client/src/context/UserProvider.tsx
@@ -1,20 +1,49 @@
 import React, { useState, FC } from 'react'
 import axios from 'axios'
-export const UserContext = React.createContext({})
+import { StringMappingType } from 'typescript'
 
+
+
+type userProviderType = {
+    userState: {
+        username: string,
+        password: string,
+    }
+    setUserState: React.Dispatch<React.SetStateAction<{
+        username: string;
+        password: string;
+    }>>
+    
+}
+
+const UserContext = React.createContext<userProviderType>({} as userProviderType)
 
 export const UserProvider: FC = ({ children }) => {
     
+    const initState = {
+        username: "TEST USERNAME",
+        password: "TEST PASSWORD",
+    }
 
+    // const initState = {
+    //     user: JSON.parse(localStorage.getItem("user")) || {},
+    //     token: localStorage.getItem("token") || "",
+    //     issues: [],
+    //     errMsg: ""
+    // }
 
+    const [userState, setUserState] = useState(initState)
 
     return (
         <UserContext.Provider 
             value={{
-
+                userState,
+                setUserState
             }}>
 
             {children}
         </UserContext.Provider>
     )
 }
+
+export const useUserContext = () => React.useContext(UserContext)
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
+import { useUserContext } from '../context/UserProvider'
+
 function Home() {
 
     const navigate = useNavigate()
+    const { userState: {
+        username,
+        password
+    } } = useUserContext()
 
     return (
         <div>
-            <h1>Hello Username</h1>
+            <h1>Hello {username}</h1>
+            <p>Your password is {password}</p>
             <button onClick={() => {
                 navigate('/clientdashboard')
             }}>Client Dashboard</button>
