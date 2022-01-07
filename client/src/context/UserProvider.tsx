46093f7d89b07234b0a67baaf29be2b17b50c7b7
Noah Weaver <noahweaver@aol.com>
2022-01-07 17:54:30 -0600

    removed typing for ease of use....may add back later after app is built

--- client/src/context/UserProvider.tsx
+++ client/src/context/UserProvider.tsx
@@ -1,45 +1,97 @@
-import React, { useState, FC } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
-import { StringMappingType } from 'typescript'
+const userAxios = axios.create()
+
+// type userStateType = {
+//         user: any,
+//         token: string,
+//         errMsg: string
+// }
+
+// type userProviderType = {
+//     userState: userStateType,
+//     setUserState: React.Dispatch<React.SetStateAction<userStateType>>,
+//     signup: (credentials: any) => void,
+//     login: (credentials: any) => void,
+// }
+
+const UserContext = React.createContext({})
+// const UserContext = React.createContext<userProviderType>({} as userProviderType)
 
 
 
-type userProviderType = {
-    userState: {
-        username: string,
-        password: string,
+userAxios.interceptors.request.use(config => {
+    const token = localStorage.getItem("token") || "";
+    if(config.headers) {
+        config.headers.Authorization = `Bearer ${token}`;
     }
-    setUserState: React.Dispatch<React.SetStateAction<{
-        username: string;
-        password: string;
-    }>>
-    
-}
+    return config;
+});
 
-const UserContext = React.createContext<userProviderType>({} as userProviderType)
-
-export const UserProvider: FC = ({ children }) => {
+export const UserProvider: React.FC = ({ children }) => {
     
     const initState = {
-        username: "TEST USERNAME",
-        password: "TEST PASSWORD",
+        user: JSON.parse(localStorage.getItem("user") || "") || {},
+        token: localStorage.getItem("token") || "",
+        errMsg: ""
     }
-
-    // const initState = {
-    //     user: JSON.parse(localStorage.getItem("user")) || {},
-    //     token: localStorage.getItem("token") || "",
-    //     issues: [],
-    //     errMsg: ""
-    // }
 
     const [userState, setUserState] = useState(initState)
 
+//Signup
+    function signup(credentials: string){
+        axios.post("/auth/signup", credentials)
+            .then(res => {
+                const {user, token} = res.data
+                localStorage.setItem("token", token)
+                localStorage.setItem("user", JSON.stringify(user))
+                setUserState(prev => ({
+                    ...prev,
+                    user,
+                    token
+                }))
+                // localStorage.setItem("userState", userState)
+            })
+            .catch(err => console.dir(err))
+            // .catch(err => handleAuthErr(err.response.data.errMsg))
+            // .catch(err => console.dir(err))
+    }
+    //Login
+    function login(credentials: string){
+        axios.post("/auth/login", credentials)
+            .then(res => {
+                console.log(res)
+                const {user, token} = res.data
+                localStorage.setItem("token", token)
+                localStorage.setItem("user", JSON.stringify(user))
+                setUserState(prevUserState => ({
+                    ...prevUserState,
+                    user,
+                    token
+                }))
+                
+            })
+            .catch(err => console.dir(err))
+            // .catch(err => handleAuthErr(err.response.data.errMsg))
+    }
+//logout
+// function logout(){
+//     localStorage.removeItem("token")
+//     localStorage.removeItem("user")
+//     localStorage.removeItem("User Issues")
+//     setUserState({
+//         user: {},
+//         token: ""
+//     })
+// }
     return (
-        <UserContext.Provider 
-            value={{
-                userState,
-                setUserState
-            }}>
+        <UserContext.Provider
+            value ={{
+                ...userState,
+                setUserState,
+                signup,
+                login
+            }} >
 
             {children}
         </UserContext.Provider>
