a65e9e192093d9b801187f4400f63a351ccbc5c5
Noah Weaver <noahweaver@aol.com>
2022-01-07 17:53:08 -0600

    added typing for needed context import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,13 +3,19 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
 import ClientDashboard from './pages/ClientDashboard';
 import Footer from './components/Footer'
+import { useUserContext } from './context/UserProvider';
 
-
-
-
-
+type userStateType = {
+  user: any,
+  token: string,
+  errMsg: string,
+  signup: (credentials: any) => void
+}
 
 function App() {
+
+  const { token, signup } = useUserContext() as userStateType
+
   return (
     <>
       <Routes>
